6e13aa60833bf6445a2c12a680c2bb03293daf3a
Pawel Wlazlo <mrogur@gmail.com>
2013-05-26 11:59:37 +0200

    TemplateFile: added file load

--- IdeTools.py
+++ IdeTools.py
@@ -25,9 +25,21 @@ class IdeToolsCommand(sublime_plugin.WindowCommand):
                 self.root = root
                 self.relativePath = relativePath
                 self.filename = filename
+                self.targetPath = None
+                self.fileObj = None
 
             def __repr__(self):
                 return os.path.join(self.root, self.relativePath, self.filename)
+
+            def read(self):
+                if not self.targetPath:
+                    raise IOError("Target path is not set")
+                try:    
+                    self.fileObj = open(os.path.join(self.targetPath, self.filename), encoding='utf-8')
+                    return self.fileObj.read()
+                except IOError as e:
+                    print(e)
+                    return False   
 
         class Template(object):
             def __init__(self, context, name, projectPath):
@@ -57,7 +69,6 @@ class IdeToolsCommand(sublime_plugin.WindowCommand):
             def scanTemplateFiles(self):
                 for (root, dirs, files) in os.walk(self.sublimePackagesTemplatesPath):
                     for file in files:
-                        # print(file)
                         self.files.append(self.getTemplateFile(root, file))
 
                 pprint(self.files)            
@@ -75,21 +86,30 @@ class IdeToolsCommand(sublime_plugin.WindowCommand):
                         root = d 
 
                 return TemplateFile(root, relativePath, file)        
-                            
-                            
-
-
-
-
-            def copyFiles(self):
-                if not (len(self.files) and self.projectPath):
-                    raise ValueError("No files in template or project path not set")
-
+            
+            def copyTemplateFiles(self):
                 for file in self.files:
-                    shutil.copy(str(file), self.projectPath)        
+                    targetDir = os.path.join(self.projectPath, file.relativePath)
+                    if not os.path.exists(targetDir):
+                        os.makedirs(targetDir)                
+                    shutil.copy(str(file), targetDir) 
+                    file.targetPath = targetDir           
 
+            def process(self):
+                for f in self.files:
+                    print(f)
+                    buffer = f.read()
+                    if buffer:
+                        print(buffer)
+
+                        
+
+        
+                
         template = Template('php','composer', '/Users/mrogur/Code/test/vc')
         template.scanTemplateFiles()
+        template.copyTemplateFiles()
+        template.process()
         #template.copyFiles()
         # def mu(result):
         #     print(result)
---
+++ templates/php/composer/bootstrap.php
@@ -0,0 +1,1 @@
+<?php echo "Original bootstrap"
